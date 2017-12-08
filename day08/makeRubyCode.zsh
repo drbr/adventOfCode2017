1a1a718e06295b063635c24838077e399e4e94d5
Andrew Brandon <drbr@users.noreply.github.com>
2017-12-07 22:33:37 -0800

    Day 8 in Ruby

---
+++ day08/makeRubyCode.zsh
@@ -0,0 +1,20 @@
+#/bin/zsh
+
+# Instructions:
+# ./makeRubyCode.zsh input.txt > day08.rb
+# ruby day08.rb
+
+filename=$1
+
+awk '{print $1" = 0"}' < $filename
+awk '{print $5" = 0"}' < $filename
+
+echo globalMax = -1.0/0.0
+echo endMax = -1.0/0.0
+
+sed 's/inc/+=/g' < $filename | sed 's/dec/-=/g' | awk '{print $0"\nglobalMax = "$1" if "$1" > globalMax"}'
+
+awk '{print "endMax = "$1" if "$1" > endMax"}' < $filename
+
+echo puts endMax
+echo puts globalMax
